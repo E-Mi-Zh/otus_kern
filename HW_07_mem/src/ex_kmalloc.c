e40d3226f2ff905244583c7133ce543bf8e7ab64
e.mi.zh <e.mi.zh@yandex.ru>
2025-09-30 07:33:00 +0300

    hw07: kmalloc - remove multiple allocation test

--- HW_07_mem/src/ex_kmalloc.c
+++ HW_07_mem/src/ex_kmalloc.c
@@ -6,8 +6,6 @@
 #include <linux/slab.h>
 #include <linux/ktime.h>
 
-#define NUM_ALLOCS 100
-
 /* Module parameter for allocation size in KB */
 static unsigned int alloc_size_kb = 4;  /* Default: 4KB */
 module_param(alloc_size_kb, uint, 0644);
@@ -18,7 +16,6 @@ static void test_kmalloc_allocation(void)
 	void *ptr;
 	ktime_t start_time, end_time;
 	s64 alloc_time_ns;
-	int i;
 
 	pr_info("kmalloc: %d byte\n", alloc_size_kb * 1024);
 
@@ -36,20 +33,6 @@ static void test_kmalloc_allocation(void)
 	alloc_time_ns = ktime_to_ns(ktime_sub(end_time, start_time));
 	pr_info("kmalloc: %d byte, %lld ns, type: PHYSICALLY_CONTIGUOUS\n",
 		alloc_size_kb * 1024, alloc_time_ns);
-
-	/* Test multiple allocations */
-	start_time = ktime_get();
-	for (i = 0; i < NUM_ALLOCS; i++) {
-		void *temp_ptr = kmalloc(alloc_size_kb * 1024, GFP_KERNEL);
-		if (temp_ptr) {
-			kfree(temp_ptr);
-		}
-	}
-	end_time = ktime_get();
-	alloc_time_ns =
-		ktime_to_ns(ktime_sub(end_time, start_time)) / NUM_ALLOCS;
-	pr_info("kmalloc: multiple alloc/free test. %d operations, avg time: %lld ns\n",
-		i, alloc_time_ns);
 }
 
 static int __init kmalloc_module_init(void)
