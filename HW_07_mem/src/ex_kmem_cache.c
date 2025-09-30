08d560853fb6d7120162869a45830eecc7a33118
e.mi.zh <e.mi.zh@yandex.ru>
2025-09-30 14:41:35 +0300

    hw07: ex_kmem_cache - add parameters, remove multiple alloc

--- HW_07_mem/src/ex_kmem_cache.c
+++ HW_07_mem/src/ex_kmem_cache.c
@@ -8,21 +8,30 @@
 
 #define CACHE_SIZE 1024
 #define NUM_OBJECTS 1024
-#define NUM_ALLOCS 100
+
+/* Module parameter for cache size in KB */
+static unsigned int cache_size_kb = 1; /* Default: 1KB */
+module_param(cache_size_kb, uint, 0644);
+MODULE_PARM_DESC(cache_size_kb, "Cache size in kilobytes (default: 1)");
+
+/* Module parameter for number of objects in cache */
+static unsigned int num_objs = 1; /* Default: 1 */
+module_param(num_objs, uint, 0644);
+MODULE_PARM_DESC(num_objs, "Number of objects in cache (default: 1)");
 
 static struct kmem_cache *my_cache;
-static void *objects[NUM_OBJECTS];
+static void **objects;
 
 static void test_kmem_cache_allocation(void)
 {
 	ktime_t start_time, end_time;
 	s64 alloc_time_ns;
-	int i, j;
+	int i;
 
 	pr_info("kmem_cache: creating cache with object size %d byte\n",
-		CACHE_SIZE);
+		cache_size_kb * 1024);
 
-	my_cache = kmem_cache_create("my_test_cache", CACHE_SIZE, 0,
+	my_cache = kmem_cache_create("my_test_cache", cache_size_kb * 1024, 0,
 				     SLAB_HWCACHE_ALIGN, NULL);
 	if (!my_cache) {
 		pr_err("kmem_cache: FAIL, err_msg = Cache creation failed\n");
@@ -34,7 +43,7 @@ static void test_kmem_cache_allocation(void)
 	start_time = ktime_get();
 
 	/* Allocate multiple objects from the cache */
-	for (i = 0; i < NUM_OBJECTS; i++) {
+	for (i = 0; i < num_objs; i++) {
 		objects[i] = kmem_cache_alloc(my_cache, GFP_KERNEL);
 		if (!objects[i]) {
 			pr_err("kmem_cache: FAIL at object %d\n", i);
@@ -43,44 +52,29 @@ static void test_kmem_cache_allocation(void)
 	}
 
 	end_time = ktime_get();
-	alloc_time_ns =
-		ktime_to_ns(ktime_sub(end_time, start_time)) / NUM_OBJECTS;
+	alloc_time_ns = ktime_to_ns(ktime_sub(end_time, start_time)) / num_objs;
 
 	pr_info("kmem_cache: %d objects allocated, avg time per object: %lld ns, type: PHYSICALLY_CONTIGUOUS\n",
-		NUM_OBJECTS, alloc_time_ns);
+		num_objs, alloc_time_ns);
+	pr_info("kmem_cache: total memory size: %u bytes\n", num_objs * cache_size_kb * 1024);
 
 cleanup:
-	for (i = 0; i < NUM_OBJECTS; i++) {
+	for (i = 0; i < num_objs; i++) {
 		if (objects[i]) {
 			kmem_cache_free(my_cache, objects[i]);
 		}
 	}
-
-	/* Test multiple allocations */
-	start_time = ktime_get();
-	for (j = 0; j < NUM_ALLOCS; j++) {
-		for (i = 0; i < NUM_OBJECTS; i++) {
-			objects[i] = kmem_cache_alloc(my_cache, GFP_KERNEL);
-			if (!objects[i]) {
-				pr_err("kmem_cache: FAIL at iteration %d, object %d\n",
-				       j, i);
-				break;
-			}
-			kmem_cache_free(my_cache, objects[i]);
-		}
-	}
-	end_time = ktime_get();
-	alloc_time_ns = ktime_to_ns(ktime_sub(end_time, start_time)) /
-			NUM_ALLOCS / NUM_OBJECTS;
-	pr_info("kmem_cache: multiple alloc/free test. %d operations, avg time: %lld ns\n",
-		j, alloc_time_ns);
 }
 
 static int __init kmem_cache_module_init(void)
 {
 	pr_info("[INIT] %s module loaded\n", KBUILD_MODNAME);
 
-	memset(objects, 0, sizeof(objects));
+	objects = kmalloc(num_objs * sizeof(void *), GFP_KERNEL);
+	if (!objects) {
+		pr_err("[INIT]: fail to alloc objects array!\n");
+		return -ENOMEM;
+	}
 	test_kmem_cache_allocation();
 
 	return 0;
@@ -88,6 +82,9 @@ static int __init kmem_cache_module_init(void)
 
 static void __exit kmem_cache_module_exit(void)
 {
+	if (objects != NULL) {
+		kfree(objects);
+	}
 	if (my_cache) {
 		kmem_cache_destroy(my_cache);
 		pr_info("[EXIT] kmem_cache: cache destroyed\n");
